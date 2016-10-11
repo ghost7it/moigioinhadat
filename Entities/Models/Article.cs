06f749dd309a77e2feb55a4d48e2c023ea8a01d0
HuyTQ <quochuy7it@gmail.com>
2016-10-11 22:45:36 +0700

    a

--- Entities/Models/Article.cs
+++ Entities/Models/Article.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Entities.Models
 {
     /// <summary>
-    /// Bài viết
+    /// Bài viết 
     /// </summary>
     [Table("Article")]
     public class Article : Entity
