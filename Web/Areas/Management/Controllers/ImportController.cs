7153351ca753ca59145eb65812a137a4b9c0429d
HuyTQ <quochuy7it@gmail.com>
2016-11-07 02:03:12 +0700

    20161107

--- Web/Areas/Management/Controllers/ImportController.cs
+++ Web/Areas/Management/Controllers/ImportController.cs
@@ -18,6 +18,7 @@ using System.Web.Mvc;
 using Web.Areas.Management.Filters;
 using Web.Areas.Management.Helpers;
 using Web.Helpers;
+using System.Transactions;
 
 namespace Web.Areas.Management.Controllers
 {
@@ -46,7 +47,7 @@ namespace Web.Areas.Management.Controllers
 
         [HttpPost]
         [Route("import-nha")]
-        public string ImportNha(HttpPostedFileBase uploadFile)
+        public ActionResult ImportNha(HttpPostedFileBase uploadFile)
         {
             StringBuilder strValidations = new StringBuilder(string.Empty);
             try
@@ -56,8 +57,6 @@ namespace Web.Areas.Management.Controllers
                     string filePath = Path.Combine(HttpContext.Server.MapPath("/Uploads/files"), Path.GetFileName(uploadFile.FileName));
                     uploadFile.SaveAs(filePath);
                     DataSet ds = new DataSet();
-
-                    //A 32-bit provider which enables the use of
 
                     string ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties=Excel 12.0;";
 
@@ -74,9 +73,90 @@ namespace Web.Areas.Management.Controllers
                             {
                                 if (ds.Tables[0].Rows.Count > 0)
                                 {
-                                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                                    Nha nha = new Nha();
+
+                                    int resultCount = 0;
+                                    int recordExcelCount = ds.Tables[0].Rows.Count;
+
+                                    using (TransactionScope tscope = new TransactionScope())
                                     {
-                                        //Now we can insert this data to database...  
+                                        foreach (DataRow r in ds.Tables[0].Rows)
+                                        {
+                                            string matBang = Convert.ToString(r["Mặt bằng"]);
+                                            long matBangId = (_repository.GetRepository<MatBang>().Read(o => o.Name == matBang)).Id;
+
+                                            string quan = Convert.ToString(r["Quận"]);
+                                            long quanId = (_repository.GetRepository<Quan>().Read(o => o.Name == quan)).Id;
+
+                                            string duong = Convert.ToString(r["Đường"]);
+                                            long duongId = (_repository.GetRepository<Duong>().Read(o => o.Name == duong)).Id;
+
+                                            string noiThatKhachThueCu = Convert.ToString(r["Nội thất khách thuê cũ"]);
+                                            long noiThatKhachThueCuId = (_repository.GetRepository<NoiThatKhachThueCu>().Read(o => o.Name == noiThatKhachThueCu)).Id;
+
+
+                                            string danhGiaPhuHopVoi = Convert.ToString(r["Đánh giá phù hợp với"]);
+                                            long danhGiaPhuHopVoiId = (_repository.GetRepository<DanhGiaPhuHopVoi>().Read(o => o.Name == danhGiaPhuHopVoi)).Id;
+
+                                            string capDoTheoDoi = Convert.ToString(r["Cấp độ theo dõi"]);
+                                            int capDoTheoDoiId = (_repository.GetRepository<CapDoTheoDoi>().Read(o => o.Name == capDoTheoDoi)).Id;
+
+                                            nha.MatBangId = matBangId;
+                                            nha.QuanId = quanId;
+                                            nha.DuongId = duongId;
+                                            nha.SoNha = r["Số nhà"] == DBNull.Value ? "" : r["Số nhà"].ToString();
+                                            nha.TenToaNha = r["Tên toàn nhà"] == DBNull.Value ? "" : (string)r["Tên toàn nhà"];
+                                            nha.MatTienTreoBien = r["Mặt tiền treo biển"] == DBNull.Value ? 0 : float.Parse(r["Mặt tiền treo biển"].ToString());
+                                            nha.BeNgangLotLong = r["Bề ngang lọt lòng"] == DBNull.Value ? 0 : float.Parse(r["Bề ngang lọt lòng"].ToString());
+                                            nha.DienTichDat = r["Diện tích đất"] == DBNull.Value ? 0 : float.Parse(r["Diện tích đất"].ToString());
+                                            nha.DienTichDatSuDungTang1 = r["Diện tích đất sử dụng tầng 1"] == DBNull.Value ? 0 : float.Parse(r["Diện tích đất sử dụng tầng 1"].ToString());
+                                            nha.SoTang = r["Số tầng"] == DBNull.Value ? 0 : int.Parse(r["Số tầng"].ToString());
+                                            nha.TongDienTichSuDung = r["Tổng diện tích sử dụng"] == DBNull.Value ? 0 : int.Parse(r["Tổng diện tích sử dụng"].ToString());
+                                            nha.DiChungChu = (string)r["Đi chung chủ"] == "Có" ? true : false;
+                                            nha.Ham = (string)r["Hầm"] == "Có" ? true : false;
+                                            nha.ThangMay = (string)r["Thang máy"] == "Có" ? true : false;
+                                            nha.NoiThatKhachThueCuId = noiThatKhachThueCuId;
+                                            nha.DanhGiaPhuHopVoiId = danhGiaPhuHopVoiId;
+                                            nha.TongGiaThue = r["Tổng giá thuê"] == DBNull.Value ? 0 : decimal.Parse(r["Tổng giá thuê"].ToString());
+                                            nha.GiaThueBQ = r["Giá thuê BQ"] == DBNull.Value ? 0 : decimal.Parse(r["Giá thuê BQ"].ToString());
+                                            nha.TenNguoiLienHeVaiTro = r["Tên người liên hệ - vai trò"] == DBNull.Value ? "" : (string)r["Tên người liên hệ - vai trò"];
+                                            nha.SoDienThoai = r["Số điện thoại"] == DBNull.Value ? "" : r["Số điện thoại"].ToString();
+                                            nha.NgayCNHenLienHeLai = r["Ngày CN hẹn liên hệ lại"] == DBNull.Value ? (DateTime?)null : Convert.ToDateTime(r["Ngày CN hẹn liên hệ lại"]);
+                                            nha.CapDoTheoDoiId = capDoTheoDoiId;
+                                            nha.GhiChu = r["Ghi chú"] == DBNull.Value ? "" : (string)r["Ghi chú"];
+                                            nha.NgayTao = DateTime.Now;
+                                            nha.NguoiTaoId = AccountId;
+                                            nha.TrangThai = 0; //Chờ duyệt
+
+                                            int result = 0;
+                                            try
+                                            {
+                                                result = _repository.GetRepository<Nha>().Create(nha, AccountId);
+                                            }
+                                            catch { }
+                                            if (result > 0)
+                                            {
+                                                resultCount++;
+                                            }
+
+                                        }
+
+                                        if (resultCount == recordExcelCount)
+                                        {
+                                            tscope.Complete();
+
+                                            TempData["Success"] = "Import dữ liệu thành công!";
+
+                                            return RedirectToAction("Index");
+                                        }
+                                        else
+                                        {
+                                            Transaction.Current.Rollback();
+                                            tscope.Dispose();
+
+                                            TempData["Error"] = "Import dữ liệu không thành công, vui lòng thử lại!";
+                                            return RedirectToAction("Index");
+                                        }
                                     }
                                 }
                             }
@@ -84,8 +164,12 @@ namespace Web.Areas.Management.Controllers
                     }
                 }
             }
-            catch (Exception ex) { }
-            return "";
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Import dữ liệu không thành công, vui lòng thử lại!";
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
         }
 
         /// <summary>
