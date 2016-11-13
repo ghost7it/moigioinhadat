aaec68f7a21ad9aaf99f3239d44bfc1b13df7b55
HuyTQ <quochuy7it@gmail.com>
2016-11-13 10:54:44 +0700

    Add controller

---
+++ Web/Areas/Management/Controllers/ExportController.cs
@@ -0,0 +1,200 @@
+﻿using Common;
+using Entities.Enums;
+using Entities.Models;
+using Entities.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Web.Areas.Management.Filters;
+using Web.Areas.Management.Helpers;
+using Web.Helpers;
+using System.Transactions;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+
+namespace Web.Areas.Management.Controllers
+{
+    [RouteArea("Management", AreaPrefix = "quan-ly")]
+    [RoutePrefix("export")]
+    public class ExportController : BaseController
+    {
+        [Route("export-du-lieu", Name = "ExportIndex")]
+        [ValidationPermission(Action = ActionEnum.Read, Module = ModuleEnum.Import)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Export
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route("export-nha", Name = "ExportNha")]
+        public ActionResult ExportNha()
+        {
+            var nha = _repository.GetRepository<Nha>().GetAll()
+                                                       .Join(_repository.GetRepository<Quan>().GetAll(), b => b.QuanId, e => e.Id, (b, e) => new { Nha = b, Quan = e })
+                                                       .Join(_repository.GetRepository<Duong>().GetAll(), b => b.Nha.DuongId, g => g.Id, (b, g) => new { Nha = b, Duong = g })
+                                                       .Join(_repository.GetRepository<MatBang>().GetAll(), b => b.Nha.Nha.MatBangId, y => y.Id, (b, y) => new { Nha = b, MatBang = y })
+                                                       .Join(_repository.GetRepository<NoiThatKhachThueCu>().GetAll(), b => b.Nha.Nha.Nha.NoiThatKhachThueCuId, k => k.Id, (b, k) => new { Nha = b, NoiThatKhachThueCu = k })
+                                                       .Join(_repository.GetRepository<DanhGiaPhuHopVoi>().GetAll(), b => b.Nha.Nha.Nha.Nha.DanhGiaPhuHopVoiId, l => l.Id, (b, l) => new { Nha = b, DanhGiaPhuHopVoi = l })
+                                                       .Join(_repository.GetRepository<CapDoTheoDoi>().GetAll(), b => b.Nha.Nha.Nha.Nha.Nha.CapDoTheoDoiId, m => m.Id, (b, m) => new { Nha = b, CapDoTheoDoi = m }).ToList();
+
+            var data = nha.Select(
+            p => new
+            {
+                MatBang = p.Nha.Nha.Nha.MatBang.Name,
+                Quan = p.Nha.Nha.Nha.Nha.Nha.Quan.Name,
+                Duong = p.Nha.Nha.Nha.Nha.Duong.Name,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.SoNha,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.TenToaNha,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.MatTienTreoBien,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.BeNgangLotLong,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.DienTichDat,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.DienTichDatSuDungTang1,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.SoTang,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.TongDienTichSuDung,
+                DiChungChu = p.Nha.Nha.Nha.Nha.Nha.Nha.DiChungChu == true ? "Có" : "Không",
+                Ham = p.Nha.Nha.Nha.Nha.Nha.Nha.Ham == true ? "Có" : "Không",
+                ThangMay = p.Nha.Nha.Nha.Nha.Nha.Nha.ThangMay == true ? "Có" : "Không",
+                NoiThatKhachThueCu = p.Nha.Nha.NoiThatKhachThueCu.Name,
+                DanhGiaPhuHopVoi = p.Nha.DanhGiaPhuHopVoi.Name,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.TongGiaThue,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.GiaThueBQ,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.TenNguoiLienHeVaiTro,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.SoDienThoai,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.NgayCNHenLienHeLai,
+                CapDoTheoDoi = p.CapDoTheoDoi.Name,
+                p.Nha.Nha.Nha.Nha.Nha.Nha.GhiChu
+            });
+
+            GridView gridview = new GridView();
+            gridview.DataSource = data;
+            gridview.DataBind();
+
+            // Clear all the content from the current response
+            Response.ClearContent();
+            Response.Buffer = true;
+
+            // set the header
+            Response.AddHeader("content-disposition", "attachment; filename=nha_data_export.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+
+            // create HtmlTextWriter object with StringWriter
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter htw = new HtmlTextWriter(sw))
+                {
+                    //set font
+                    Response.ContentEncoding = System.Text.Encoding.UTF8;
+                    htw.WriteLine("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">");
+
+                    // render the GridView to the HtmlTextWriter
+                    gridview.RenderControl(htw);
+
+                    // Output the GridView content saved into StringWriter
+                    Response.Output.Write(sw.ToString());
+                    Response.Flush();
+                    Response.End();
+                }
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// Export
+        /// </summary>
+        /// <returns></returns>
+        [Route("export-khach", Name = "ExportKhach")]
+        public ActionResult ExportKhach()
+        {
+
+            var khachVaNhuCauThue = _repository.GetRepository<NhuCauThue>().GetAll()
+                                                       .Join(_repository.GetRepository<Quan>().GetAll(), b => b.QuanId, e => e.Id, (b, e) => new { NhuCauThue = b, Quan = e })
+                                                       .Join(_repository.GetRepository<Duong>().GetAll(), b => b.NhuCauThue.DuongId, g => g.Id, (b, g) => new { NhuCauThue = b, Duong = g })
+                                                       .Join(_repository.GetRepository<MatBang>().GetAll(), b => b.NhuCauThue.NhuCauThue.MatBangId, y => y.Id, (b, y) => new { NhuCauThue = b, MatBang = y })
+                                                       .Join(_repository.GetRepository<NoiThatKhachThueCu>().GetAll(), b => b.NhuCauThue.NhuCauThue.NhuCauThue.NoiThatKhachThueCuId, k => k.Id, (b, k) => new { NhuCauThue = b, NoiThatKhachThueCu = k })
+                                                       .Join(_repository.GetRepository<Khach>().GetAll(), b => b.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.KhachId, n => n.Id, (b, n) => new { NhuCauThue = b, Khach = n })
+                                                       .Join(_repository.GetRepository<CapDoTheoDoi>().GetAll(), b => b.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.CapDoTheoDoiId, m => m.Id, (b, m) => new { NhuCauThue = b, CapDoTheoDoi = m }).ToList();
+
+            var data = khachVaNhuCauThue.Select(
+            p => new
+            {
+                //Khách
+                TenNguoiLienHeVaiTro = p.NhuCauThue.Khach.TenNguoiLienHeVaiTro,
+                SoDienThoai = p.NhuCauThue.Khach.SoDienThoai,
+                GhiChuKhach = p.NhuCauThue.Khach.GhiChu,
+
+                //Nhu Cầu Thuê
+                MatBang = p.NhuCauThue.NhuCauThue.NhuCauThue.MatBang.Name,
+                Quan = p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.Quan.Name,
+                Duong = p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.Duong.Name,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.SoNha,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.TenToaNha,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.MatTienTreoBien,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.BeNgangLotLong,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.DienTichDat,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.DienTichDatSuDungTang1,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.SoTang,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.TongDienTichSuDung,
+                DiChungChu = p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.DiChungChu == true ? "Có" : "Không",
+                Ham = p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.DiChungChu == true ? "Có" : "Không",
+                ThangMay = p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.DiChungChu == true ? "Có" : "Không",
+                NoiThatKhachThueCu = p.NhuCauThue.NhuCauThue.NoiThatKhachThueCu.Name,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.TongGiaThue,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.GiaThueBQ,
+                p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NgayCNHenLienHeLai,
+                CapDoTheoDoi = p.CapDoTheoDoi.Name,
+                GhiChuNhuCauThue = p.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.NhuCauThue.GhiChu,
+            });
+
+            GridView gridview = new GridView();
+            gridview.DataSource = data;
+            gridview.DataBind();
+
+            // Clear all the content from the current response
+            Response.ClearContent();
+            Response.Buffer = true;
+
+            // set the header
+            Response.AddHeader("content-disposition", "attachment; filename=khach_data_export.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+
+            // create HtmlTextWriter object with StringWriter
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter htw = new HtmlTextWriter(sw))
+                {
+                    //set font
+                    Response.ContentEncoding = System.Text.Encoding.UTF8;
+                    htw.WriteLine("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">");
+
+                    // render the GridView to the HtmlTextWriter
+                    gridview.RenderControl(htw);
+
+                    // Output the GridView content saved into StringWriter
+                    Response.Output.Write(sw.ToString());
+                    Response.Flush();
+                    Response.End();
+                }
+            }
+            return View();
+        }
+
+
+
+    }
+}
