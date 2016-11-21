772f766e838c2c2de17103324f4575067c2bbb8a
HuyTQ <quochuy7it@gmail.com>
2016-11-22 01:22:32 +0700

    AAA

--- Entities/Enums/ModuleEnum.cs
+++ Entities/Enums/ModuleEnum.cs
@@ -90,6 +90,7 @@ namespace Entities.Enums
         [Description("Chuyển giao dữ liệu")]
         [ActionAttribute(ActionType.Read, ActionType.Update)]
         ChuyenGiaoDuLieu = 454
+     
         #endregion
         #endregion
     }
